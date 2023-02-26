b4e46d217f2fc93aed7120cbdaea33c747fa619c
ryanenoch <105472843+ryanenoch@users.noreply.github.com>
2023-02-25 22:35:39 -0500

    Revert "Update price.py"
    This reverts commit 5df4f3f6836d2d7402e153f4ac1262100194d89c.

--- price.py
+++ price.py
@@ -1,4 +1,4 @@
-def price(name,store):
+def price(name):
   import duckdb
   import pandas as pd
 
@@ -7,7 +7,7 @@ def price(name,store):
 
   #Table created only if it doesnt exist, data copied from CSV to DB
   conn.execute(
-    "CREATE TABLE IF NOT EXISTS price_data AS SELECT * FROM 'X2grocery_price.csv'"
+    "CREATE TABLE IF NOT EXISTS price_data AS SELECT * FROM 'Xgrocery_price.csv'"
   )
 
   conn.execute("""
@@ -21,12 +21,10 @@ def price(name,store):
                 ORDER BY max_data.date DESC
                 """)
   
-  #If at least one of the inputs(product name and/or store name) are not NoneType, either 1 of the first 3 SQL case insensitive query statement is executed otherwise the last simpler SQL statement is executed
-  
+  #If input(product name) is not NoneType, 1st SQL case insensitive query statement is executed otherwise a simpler SQL statement is executed
   if not name is None:
     name = '%' + name + '%' #adding wildcard char before and after the input
-    print(name)
-    print(store)
+    
     #View records in form of a dataframe
     result = conn.execute("""
                         SELECT 
@@ -34,76 +32,28 @@ def price(name,store):
                         ARRAY_EXTRACT(STR_SPLIT(store_product,'_'),1) AS store, 
                         ARRAY_EXTRACT(STR_SPLIT(store_product,'_'),2) AS product,
                         availability,
-                        reg_price,
-                        rew_price
+                        price
                         FROM latest_records
-                        WHERE lower(product) LIKE ? 
+                        WHERE lower(product) LIKE ?
                         """,(name.lower(),)).fetchdf()
     
    
     result['date'] = result['date'].dt.date  #fix for timestamp error
-    return result
-
-  elif not store is None:
-    store = store + '%'
-    print(name)
-    print(store)
-    #View records in form of a dataframe
-    result = conn.execute("""
-                        SELECT 
-                        date,
-                        ARRAY_EXTRACT(STR_SPLIT(store_product,'_'),1) AS store, 
-                        ARRAY_EXTRACT(STR_SPLIT(store_product,'_'),2) AS product,
-                        availability,
-                        reg_price,
-                        rew_price
-                        FROM latest_records
-                        WHERE lower(store) LIKE ?
-                        """,(store.lower(),)).fetchdf()
-    
-   
-    result['date'] = result['date'].dt.date  #fix for timestamp error
-    return result
-
-  elif not name is None or not store is None:
-    name = '%' + name + '%' #adding wildcard char before and after the input
-    store = store + '%'
-    print(name)
-    print(store)
-    #View records in form of a dataframe
-    result = conn.execute("""
-                        SELECT 
-                        date,
-                        ARRAY_EXTRACT(STR_SPLIT(store_product,'_'),1) AS store, 
-                        ARRAY_EXTRACT(STR_SPLIT(store_product,'_'),2) AS product,
-                        availability,
-                        reg_price,
-                        rew_price
-                        FROM latest_records
-                        WHERE lower(product) LIKE ? 
-                        OR lower(store) LIKE ?
-                        """,(name.lower(),store.lower())).fetchdf()
-    
-   
-    result['date'] = result['date'].dt.date  #fix for timestamp error
-    return result
+    print(result.dtypes)
+    print()
   
   else:
-   
+    #View all records in form of a dataframe
     result = conn.execute("""
                         SELECT 
                         date,
                         ARRAY_EXTRACT(STR_SPLIT(store_product,'_'),1) AS store, 
                         ARRAY_EXTRACT(STR_SPLIT(store_product,'_'),2) AS product,
                         availability,
-                        reg_price,
-                        rew_price
+                        price
                         FROM latest_records
                         """).fetchdf()
-    
-   
     result['date'] = result['date'].dt.date  #fix for timestamp error
-    return result
 
-
+  return result
 
